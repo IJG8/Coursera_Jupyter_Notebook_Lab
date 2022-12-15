023c3a6ebc17a1e4ba5bb6b9a4c05c1ac7bb33e2
IJG8 <107727213+IJG8@users.noreply.github.com>
2022-12-14 19:03:21 -0500

    Create My_Jupyter_Notebook_on_IBM_Watson_Studio.py

---
+++ My_Jupyter_Notebook_on_IBM_Watson_Studio.py
@@ -0,0 +1,30 @@
+# My Jupyter Notebook on IBM Watson Studio
+
+Hi my name is **Isaiah G** , 
+My current and desired occupation isnt clear yet. 
+*Even though my desired occupation isn't yet known I am interested in data science, because of its ablilty to help people make more effective and efficient buisness decisions.*
+### The code below adds two numbers and prints the resulting sum. 
+a = 4
+b = 3
+c = a + b
+
+print(c)
+### Horizontal line as seperator
+
+This is the content above the horizontal seperator 
+***
+This is the content below the horizontal seperator 
+
+- Item 1
+- Item 2
+- Item 3
+
+
+| This    |       is       | |      a   |
+---
+| table | |      for      | |    the     |
+---
+| final  | | coursera | | project |
+---
+
+`print(I appreciate you)`
